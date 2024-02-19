e7dad2d52a2ba336900cd35b9f9412ae191b2722
zerdan-mohamed <zerdan.mohamed@gmail.com>
2024-02-19 01:10:32 +0100

    MZN - change mysql to postgresql

--- user-service/src/main/java/com/bookstore/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/bookstore/userservice/UserServiceApplication.java
@@ -2,9 +2,10 @@ package com.bookstore.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
